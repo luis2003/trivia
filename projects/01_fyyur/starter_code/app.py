38b1898441c75e177b0fc85f629a6ff9d0a5a093
luis2003 <luis2003@gmail.com>
2021-04-11 13:12:07 -0300

    fixing!

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -46,6 +46,8 @@ class Venue(db.Model):
     website_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(120))
+    #relationship
+    show = db.relationship('Show', backref='Venue', cascade='all, delete')
 
     # (DONE?) TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -63,6 +65,9 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(120))
+    #relationship
+    show = db.relationship('Show', backref='Artist', cascade='all, delete')
+
 
     # (DONE?) TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -70,12 +75,12 @@ class Artist(db.Model):
 class Show(db.Model):
   __tablename__ = 'show'
   id = db.Column(db.Integer, primary_key=True)
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id', ondelete='CASCADE'), nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id', ondelete='CASCADE'), nullable=False)
   start_time = db.Column(db.DateTime, nullable=False)
 # relationships
-  artist = db.relationship(Artist, backref=db.backref('show', cascade='all, delete'))
-  venue = db.relationship(Venue, backref=db.backref('show', cascade='all, delete'))
+#  artist = db.relationship(Artist, backref=db.backref('show', cascade='all, delete'))
+#  venue = db.relationship(Venue, backref=db.backref('show', cascade='all, delete'))
 
 # (DONE?) TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
