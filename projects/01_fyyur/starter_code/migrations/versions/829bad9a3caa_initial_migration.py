bb2440da9fb396757a07e7bc65d736a37b238d05
luis2003 <luis2003@gmail.com>
2021-04-11 20:32:58 -0300

    final

--- projects/01_fyyur/starter_code/migrations/versions/829bad9a3caa_initial_migration.py
+++ projects/01_fyyur/starter_code/migrations/versions/829bad9a3caa_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration.
 
-Revision ID: db352d763124
+Revision ID: 829bad9a3caa
 Revises: 
-Create Date: 2021-04-07 18:30:13.625336
+Create Date: 2021-04-11 19:30:31.874172
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'db352d763124'
+revision = '829bad9a3caa'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -25,7 +25,7 @@ def upgrade():
     sa.Column('state', sa.String(length=120), nullable=True),
     sa.Column('phone', sa.String(length=120), nullable=True),
     sa.Column('genres', sa.ARRAY(sa.String()), nullable=False),
-    sa.Column('image_link', sa.String(length=500), nullable=True),
+    sa.Column('image_link', sa.String(length=500), nullable=False),
     sa.Column('website_link', sa.String(length=120), nullable=True),
     sa.Column('facebook_link', sa.String(length=120), nullable=True),
     sa.Column('seeking_venue', sa.Boolean(), nullable=True),
@@ -40,7 +40,7 @@ def upgrade():
     sa.Column('address', sa.String(length=120), nullable=True),
     sa.Column('phone', sa.String(length=120), nullable=True),
     sa.Column('genres', sa.ARRAY(sa.String()), nullable=False),
-    sa.Column('image_link', sa.String(length=500), nullable=True),
+    sa.Column('image_link', sa.String(length=500), nullable=False),
     sa.Column('facebook_link', sa.String(length=120), nullable=True),
     sa.Column('website_link', sa.String(length=120), nullable=True),
     sa.Column('seeking_talent', sa.Boolean(), nullable=True),
@@ -52,8 +52,8 @@ def upgrade():
     sa.Column('venue_id', sa.Integer(), nullable=False),
     sa.Column('artist_id', sa.Integer(), nullable=False),
     sa.Column('start_time', sa.DateTime(), nullable=False),
-    sa.ForeignKeyConstraint(['artist_id'], ['Artist.id'], ),
-    sa.ForeignKeyConstraint(['venue_id'], ['Venue.id'], ),
+    sa.ForeignKeyConstraint(['artist_id'], ['Artist.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['venue_id'], ['Venue.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
